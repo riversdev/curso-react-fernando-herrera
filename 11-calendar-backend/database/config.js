b4ff0d1d7070b242702f7f54d81a23ea7fa186f2
Alejandro Ríos <alejandroriosdev@gmail.com>
2023-04-04 20:54:28 -0600

    deploy

--- 11-calendar-backend/database/config.js
+++ 11-calendar-backend/database/config.js
@@ -3,20 +3,10 @@ import mongoose from 'mongoose'
 export const dbConnection = async () => {
     try {
         await mongoose.connect(process.env.DB_CNN)
-        // segundo parametro del connect
-        // {
-        //     useNewUrlParser: true,
-        //     useUnifiedTopology: true,
-        //     useCreateIndex: true
-        // }
 
-        console.log('db online')
+        console.log('database online')
     } catch (error) {
         console.log(error)
         throw new Error('Error al iniciar conexion con la bd')
     }
 }
-
-// mongo db user
-// mern_user
-// rJQv5tefDz4fEK13
